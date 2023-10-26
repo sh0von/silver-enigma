387db2bb50ca350aa9fdf324d6b8c12025d1d593
Shovon <minarshovon909@gmail.com>
2023-10-27 01:33:08 +0600

    Update success.php

--- web01/success.php
+++ web01/success.php
@@ -1,4 +1,10 @@
 <?php
 $flag = "CSEC{l0g1n_1s_n0t_4_g00d_1d34}";
+
+// Check if the request comes from index.php
+if (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], 'index.php') !== false) {
+    echo '<div class="alert alert-success">Login successful! Here\'s the flag: ' . $flag . '</div>';
+} else {
+    echo 'HEHE NOTHING HERE';
+}
 ?>
-<div class="alert alert-success">Login successful! Here's the flag: <?php echo $flag; ?></div>
